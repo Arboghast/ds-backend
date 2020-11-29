1b73638631ed71cdb39e8d15b6000588d5b62e07
Jesse Huang <jesse.huang1205@gmail.com>
2020-11-28 23:38:32 -0500

    Add files via upload

--- lobbies.js
+++ lobbies.js
@@ -1,43 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+//GET localhost:port/api/lobbies/
 router.get('/',(req,res) => {
-	res.send("Lobby table");
+	
 )}
 
+//GET localhost:3000/api/lobbies/1
 router.get('/startGame/:lobbycode',(req,res) => {
-	var id = req.params.id;
+	
 })
 
-
+//POST localhost:3000/api/lobbies/john/1
 router.post('/createLobby/:guest/:lobbycode',(req,res) => {
-	var lobby = {
-		
-		"lobby":{
-
-			"host": req.params.host,
-			"id": req.params.id
-		
-		}
-		
-	};
+	
 })
 
+//PUT localhost:3000/api/lobbies/john/1
 router.put('/joinLobby/:guest/:lobbycode',(req,res) => {
 	
 })
 
+//PUT localhost:3000/api/lobbies/john/1
 router.put('/leaveLobby/:guest/:lobbycode',(req,res) => {
 	
 })
 
+//PUT localhost:3000/api/lobbies/john/1
 router.put('/kickUser/:guest/:lobbycode',(req,res) => {
-	var user = req.params.user;
-	var id = req.params.id;
+	
 })
 
+//PUT localhost:3000/api/lobbies/1
 router.delete('/removeLobby/:lobbycode',(req,res) => {
-	var id = req.params.id;
+	
 })
 
 module.exports = router;
