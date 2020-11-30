b8a35cd1fa29841fd65cc92025208c514f3318b1
Jesse Huang <jesse.huang1205@gmail.com>
2020-11-30 14:34:24 -0500

    Add files via upload

--- Router/Temp.js
+++ Router/Temp.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const router = express.Router();
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '34.70.48.83',
+  user: 'a',
+  password: 'a',
+  database: 'temp'
+})
+connection.connect();
 //POST - Adds a user to the Users Table
 router.post('/addUser', (req,res) => {
     //{id: ip&mac-based hash}
+    connection.query("INSERT INTO user (User) VALUES ('" + req.body.id + "')", function (err,entry) {
+        if (err) throw err
+
+        console.log("user created");
+        res.sendStatus(200);
+    });
+
+    
 });
 
 
 //DELETE - Deletes a user from the Users Table
 router.delete('/deleteUser',(req,res) => {
-	
+
+    connection.query("DELETE FROM user WHERE User='" + req.body.id + "'", function (err,rows,fields){
+        if(err) throw err
+
+        console.log("user deleted");
+        res.sendStatus(200);
+    });
+    
 });
 
 //POST - Adds a writing prompt to the Prompt Table 
 router.post('/addPrompt', (req,res) => {
 
+    let data = req.body;
+    connection.query("INSERT INTO prompts (Number, Text) VALUES (" + data.Number +",'"+ data.Text + "');",function (err){
+        if(err) throw err
+
+        console.log("Prompt inserted");
+        res.sendStatus(200);
+    });
+
 });
 
 //DELETE - Deletes a writing prompt from the Prompt Table
 router.delete('/deletePrompt', (req,res) => {
+
+    connection.query("DELETE FROM prompts WHERE Text=" + "'" + req.body.Text + "'",function (err){
+        if(err) throw err
+
+        console.log("Prompt deleted");
+        res.sendStatus(200);
+    });
 
 });
 
