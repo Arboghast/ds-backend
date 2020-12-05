839b157771b5a8aefe76a961df0b8f5841645c17
Jesse Huang <jesse.huang1205@gmail.com>
2020-12-05 16:19:55 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const Router = require('./Router/Temp.js');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api',Router);
 
-const PORT = process.env.PORT || 4000;
+app.use(cors());
+app.use(Router);
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on Port ${PORT}`);
 });
